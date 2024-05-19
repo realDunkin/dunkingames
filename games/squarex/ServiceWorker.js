aeaa0848dd254ca3c09a48616def56e9ff945e37
realDunkin <hectorchocojmartinez@gmail.com>
2024-05-18 23:53:19 -0400

    Create ServiceWorker.js

---
+++ games/squarex/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-SquareX - Play it now at CoolmathGames.com-1.0";
+const contentToCache = [
+    "Build/SquareX for CoolMath.loader.js",
+    "Build/SquareX for CoolMath.framework.js",
+    "Build/SquareX for CoolMath.data",
+    "Build/SquareX for CoolMath.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
