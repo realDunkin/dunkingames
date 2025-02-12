17d046a30d98945f15109c81ab036415455bc00d
realDunkin <hectorchocojmartinez@gmail.com>
2025-02-12 00:48:32 -0500

    Update main.js

--- games/big-flappy-tower-tiny-square/scripts/main.js
+++ games/big-flappy-tower-tiny-square/scripts/main.js
@@ -411,8 +411,7 @@ window.DOMElementHandler = class extends self.DOMHandler {
         ? (this._baseUrl = d.baseUrl)
         : ((k = location.origin),
           (this._baseUrl = document.href),
-          (k = this._baseUrl.lastIndexOf("/")),
-          -1 !== k && (this._baseUrl = this._baseUrl.substr(0, k + 1)));
+          -1 !== k && (this._baseUrl = this._baseUrl));
       d.workerScripts && (this._workerScriptURLs = d.workerScripts);
       k = new MessageChannel();
       this._messageChannelPort = k.port1;
